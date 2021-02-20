9b4da287042c23a0b2026b959696274900d69fc1
Joblad <10959420@uvu.edu>
2021-02-20 16:47:18 -0700

    Animal Game

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topbound = 30;
+    private float lowerbound = -10;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If Animal goes past players veiw in game, remove object
+        if (transform.position.z > topbound)
+        {
+            Destroy(gameObject);
+        } else if (transform.position.z < lowerbound)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+
+        }
+
+ }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 0.5f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SpawnRandomAnimal();   
+        }
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnpos = new Vector3(Random.Range(-20, 20), 0, 20);
+
+        Instantiate(animalPrefabs[animalIndex], spawnpos,
+                animalPrefabs[animalIndex].transform.rotation);
+
+    }
+
+}
